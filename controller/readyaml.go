892b8985c4687b21a52bda39ffab90075f837c93
Raulino Neto <raulino.neto@endurance.com>
2020-01-16 09:51:40 -0300

    Prepared to read multiples includes at serverless.yml

--- controller/readyaml.go
+++ controller/readyaml.go
@@ -1,21 +1,68 @@
 package controller
 
 import (
+	"encoding/json"
 	"io/ioutil"
+	"regexp"
+	"strings"
 
 	"github.com/ghodss/yaml"
 )
 
 func ReadYaml(file string) ([]byte, error) {
-	content, err := ioutil.ReadFile(file)
+	content, err := MapServerlessFileIncludes(file)
 	if err != nil {
 		return nil, err
 	}
 
-	cj, err := yaml.YAMLToJSON(content)
+	cj, err := json.Marshal(content)
 	if err != nil {
 		return nil, err
 	}
 
 	return cj, nil
+}
+
+func MapServerlessFileIncludes(file string) (map[string]interface{}, error) {
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	contentMap := make(map[string]interface{}, 0)
+	err = yaml.Unmarshal(content, &contentMap)
+	if err != nil {
+		return nil, err
+	}
+	r, err := regexp.Compile(`\${file\((.+).yml\)}`)
+	if err != nil {
+		return nil, err
+	}
+	for i, v := range contentMap {
+		s, ok := v.(string)
+		if !ok {
+			continue
+		}
+		result := r.FindAllString(s, -1)
+		if len(result) <= 0 {
+			contentMap[i] = result
+			continue
+		}
+		contentMap[i], err = processFiles(result)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return contentMap, nil
+}
+
+func processFiles(files []string) (content map[string]interface{}, err error) {
+	for _, file := range files {
+		file = strings.Replace(file, "${file(", "", -1)
+		file = strings.Replace(file, ")}", "", -1)
+		content, err = MapServerlessFileIncludes(file)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
